daaa0de7ec3aa500da487522d7036c409dc6c178
Akintola <akinlekan28@yahoo.com>
2018-12-07 20:15:35 +0100

    added register endpoint

--- models/User.js
+++ models/User.js
@@ -2,28 +2,29 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }, 
-    date : {
-        type: Date,
-        default: Date.now()
-    },
-    token: {
-        type: String
-    },
-    expiry: {
-        type: Date
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  },
+  token: {
+    type: String
+  },
+  expiry: {
+    type: Date
+  }
+
 });
 
 module.exports = User = mongoose.model('users', UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bycrypt = require('bcryptjs');
+
+//Load input validation
+const validateRegisterInput = require('../../validation/register');
 
 //Load user model
 const User = require('../../models/User');
@@ -10,6 +14,38 @@ const User = require('../../models/User');
 //@access Public
 router.post('/register', (req, res) => {
     
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+    User.findOne({
+        email: req.body.email
+    })
+    .then(user => {
+        if(user){
+            errors.email = 'Email Already exists';
+            return res.status(400).json(errors)
+        } else {
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            })
+
+            bycrypt.genSalt(10, (err, salt) => {
+                bycrypt.hash(newUser.password, salt, (err, hash) => {
+                    if(err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err))
+                })
+            })
+        }
+    })
+    .catch(err => console.log(err))
 })
 
 
