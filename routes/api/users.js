1f8bf3b5442a50fcc9926840f55be19b35b709ec
Akintola <akinlekan28@yahoo.com>
2018-12-07 19:07:22 +0100

    finised major setup

---
+++ routes/api/users.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+//Load user model
+const User = require('../../models/User');
+
+
+//@route  Get api/users/register
+//@desc Register user route
+//@access Public
+router.post('/register', (req, res) => {
+    
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,30 @@
-const express = require('express');
+const express = require("express");
+const bodyPaser = require('body-parser');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
 
 const app = express();
+
+//Body parser middleware
+app.use(bodyPaser.urlencoded({ extended: false }));
+app.use(bodyPaser.json());
+
+//Db
+const db = require("./config/keys").mongoURI;
+
+//MongoDB connection
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+
+//Use routes
+app.use('/api/users', users);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
