e4d701c84d77947ecfc452696bdbec72d064689b
Akintola <akinlekan28@yahoo.com>
2018-12-09 01:48:46 +0100

    added login endpoint

---
+++ config/passport.js
@@ -0,0 +1,9 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const keys = require('../config/keys');
+
+const options = {};
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = keys.secretK
