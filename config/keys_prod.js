ad98b473de3ceca1600fa4be2671665fbfa39ebe
Akintola <akinlekan28@yahoo.com>
2019-01-27 20:22:42 +0100

    added config keys for production environment

---
+++ config/keys_prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: "", //Mongodb uri link
+  secretKey: "", //Secret key for passportjs auth
+  sendGridKey: "", //Sendgrid key for password reset email
+  username: "", //email username to send payslip as email
+  password: "", //email password to send payslip as email
+  smtp: "" //email smtp server to send payslip as email
+};
