6f93f7e9b86aa4318c0d74ab981bffffd84359dd
Akintola <akinlekan28@yahoo.com>
2019-02-18 17:34:46 +0100

    renamed env variable

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI, //Mongodb uri link
-  secretKey: process.env.SECRET_KEY, //Secret key for passportjs auth
+  secretKey: process.env.SECRET_OR_KEY, //Secret key for passportjs auth
   sendGridKey: process.env.SENDGRID_KEY, //Sendgrid key for password reset email
 };
--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
-const keys = require('../config/keys');
+const keys = require('./keys')
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
